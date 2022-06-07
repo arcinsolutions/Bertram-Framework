6e9a8575c409a92776fbd9cf4c439f0e933467af
Barne Bellin <barnekiel@gmail.com>
2022-06-07 14:17:00 +0200

    added readline 👀

--- src/modules/music/events/client.ts
+++ src/modules/music/events/client.ts
@@ -1,6 +1,7 @@
 import { AnyChannel, TextChannel, VoiceState } from "discord.js";
 import { client } from "../../../golden";
 import { music } from "../api";
+import * as readline from "readline";
 
 client.once("botReady", () => {
     if (client.user == null)
@@ -12,6 +13,26 @@ client.once("botReady", () => {
 //Important
 client.on("raw", (packet) => {
     music.handleVoiceUpdate(packet);
+    let rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question('Is this example useful? [y/n] ', (answer) => {
+        switch (answer.toLowerCase()) {
+            case 'y':
+                console.log('Super!');
+                break;
+            case 'n':
+                console.log('Sorry! :(');
+                break;
+            default:
+                console.log('Invalid answer!');
+        }
+        rl.close();
+    });
+
+
 })
 
 client.on("voiceStateUpdate", async (oldState: VoiceState, newState: VoiceState) => {
