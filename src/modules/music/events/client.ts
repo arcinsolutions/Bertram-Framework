05815da4f60a67d28ecbd00b02ea48a203840a56
Barne Bellin <barnekiel@gmail.com>
2022-06-09 12:13:10 +0200

    🙄

--- src/modules/music/events/client.ts
+++ src/modules/music/events/client.ts
@@ -13,24 +13,24 @@ client.once("botReady", () => {
 //Important
 client.on("raw", (packet) => {
     music.handleVoiceUpdate(packet);
-    let rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+    // let rl = readline.createInterface({
+    //     input: process.stdin,
+    //     output: process.stdout
+    // });
 
-    rl.question('Is this example useful? [y/n] ', (answer) => {
-        switch (answer.toLowerCase()) {
-            case 'y':
-                console.log('Super!');
-                break;
-            case 'n':
-                console.log('Sorry! :(');
-                break;
-            default:
-                console.log('Invalid answer!');
-        }
-        rl.close();
-    });
+    // rl.question('Is this example useful? [y/n] ', (answer) => {
+    //     switch (answer.toLowerCase()) {
+    //         case 'y':
+    //             console.log('Super!');
+    //             break;
+    //         case 'n':
+    //             console.log('Sorry! :(');
+    //             break;
+    //         default:
+    //             console.log('Invalid answer!');
+    //     }
+    //     rl.close();
+    // });
 
 
 })
