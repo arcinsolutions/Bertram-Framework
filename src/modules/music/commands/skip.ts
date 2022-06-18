64761b16e542eef87cce411e2a74cecf75a9d330
Botis <barnekiel@gmail.com>
2022-06-18 11:46:40 +0200

    improvement 😉

--- src/modules/music/commands/skip.ts
+++ src/modules/music/commands/skip.ts
@@ -9,16 +9,14 @@ import { music } from './../api/index';
 class Skip {
     @Slash("skip", { description: "Skip the current Song" })
     async skip(@SlashOption("amount", { description: "let you skip a specific amount of Songs", required: false }) amount: number, interaction: CommandInteraction) {
-        const embed = new MessageEmbed({
-            description: "Song skiped!",
-            color: "DARK_GREEN"
-        })
-
         const player = music.players.get(interaction.guild!.id);
         player?.skip(amount);
 
         interaction.reply({
-            embeds: [embed],
+            embeds: [new MessageEmbed({
+                description: "Song skiped!",
+                color: "DARK_GREEN"
+            })],
             ephemeral: true
         })
     }
