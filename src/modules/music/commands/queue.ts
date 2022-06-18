39b1c309b6a75196bd47fdfdd8e051e159378c77
Botis <barnekiel@gmail.com>
2022-06-18 11:46:58 +0200

    added Queue command | NOT done 😅

---
+++ src/modules/music/commands/queue.ts
@@ -0,0 +1,31 @@
+import { Category } from "@discordx/utilities";
+import { CommandInteraction, MessageEmbed } from "discord.js";
+import { Discord, Slash } from "discordx";
+import { music } from "../api";
+
+@Discord()
+@Category("Music")
+class Queue {
+    @Slash("queue", { description: "Show the current queue" })
+    async queue(interaction: CommandInteraction) {
+        const player = music.players.get(interaction.guild!.id);
+        if (!player)
+            return interaction.reply({
+                embeds: [new MessageEmbed({
+                    description: ":x: No player found!",
+                    color: "DARK_RED"
+                })]
+            })
+        const queue = player.queue;
+        const embed = new MessageEmbed({
+            title: "Queue",
+            description: queue.map((song, index) => `${index + 1}. ${song.title}`).join("\n"),
+            color: "DARK_GREEN"
+        })
+
+        interaction.reply({
+            embeds: [embed],
+            ephemeral: true
+        })
+    }
+}
