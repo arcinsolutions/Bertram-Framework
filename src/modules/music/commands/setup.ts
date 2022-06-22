87cfc8cdd5b0cbb8b671bea823aa71a94550d9b9
Anweisung <xd98474@gmail.com>
2022-06-22 16:50:55 +0200

    👩‍🚒 Improved setup command

--- src/modules/music/commands/setup.ts
+++ src/modules/music/commands/setup.ts
@@ -1,8 +1,7 @@
 import { Category } from "@discordx/utilities";
-import { CommandInteraction, MessageActionRow, MessageButton, ButtonInteraction, MessageEmbed, OverwriteResolvable, Collection } from "discord.js";
+import { CommandInteraction, MessageActionRow, MessageButton, ButtonInteraction, MessageEmbed } from "discord.js";
 import { Discord, Slash, ButtonComponent } from "discordx";
 import { musicGuild as GuildEntity } from "../database/entities/guild"
-import { Guild } from "discord.js";
 import { createMusicChannel } from "../api";
 
 @Discord()
@@ -18,11 +17,64 @@ class Setup {
 
         if (interaction.guild!.channels.cache.get(dbGuild!.channelId) === undefined) {
             const channel = await createMusicChannel(interaction.guild!);
-            return interaction.editReply(`Channel created ${channel}`);
+            return interaction.editReply({
+                embeds: [new MessageEmbed({
+                    color: "#45C16C",
+                    fields: [
+                        {
+                            name: "<:tick1:989179973657051197><:tick2:989179972373585951>\n<:tick3:989179970729435136><:tick4:989179968904904774>",
+                            value: "\u200B",
+                            inline: true,
+                        },
+                        {
+                            name: "Successfully created",
+                            value:
+                                `You're all set up! Ready to jam some music? Just paste any song-, playlist link or just search for anything in here ${channel}`,
+                            inline: true,
+                        },
+                        {
+                            name: "\u200B",
+                            value: "\u200B",
+                            inline: true,
+                        }
+                    ]
+                })],
+                components: [
+                    new MessageActionRow({
+                        components: [
+                            new MessageButton({
+                                customId: "abort",
+                                style: "SUCCESS",
+                                label: "Finish",
+                            })
+                        ]
+                    })
+                ]
+            })
         }
 
         return interaction.editReply({
-            content: `Es exestiert bereits ein Channel (<#${dbGuild?.channelId}>). Möchtest du einen neuen Channel generieren?`,
+            embeds: [new MessageEmbed({
+                color: "#ED4245",
+                fields: [
+                    {
+                        name: "<:help1:989179967130726480><:help2:989179965209735188>\n<:help3:989179963607486524><:help4:989179961917186108>",
+                        value: "\u200B",
+                        inline: true,
+                    },
+                    {
+                        name: "Channel creation",
+                        value:
+                            `There already exists a channel on this server <#${dbGuild?.channelId}>\nBut you can create a new one at anytime.`,
+                        inline: true,
+                    },
+                    {
+                        name: "\u200B",
+                        value: "\u200B",
+                        inline: true,
+                    }
+                ]
+            })],
             components: [
                 new MessageActionRow({
                     components: [
@@ -46,7 +98,7 @@ class Setup {
 
     @ButtonComponent("abort")
     async abort(interaction: ButtonInteraction) {
-        return interaction.message.delete().catch();
+        return interaction.message.delete();
     }
 
     @ButtonComponent("create")
@@ -56,9 +108,39 @@ class Setup {
 
         const channel = await createMusicChannel(interaction.guild!)
         return interaction.update({
-            content: `JOO LÜPPT! ${channel}`,
-            components: []
-        });
+            embeds: [new MessageEmbed({
+                color: "#45C16C",
+                fields: [
+                    {
+                        name: "<:tick1:989179973657051197><:tick2:989179972373585951>\n<:tick3:989179970729435136><:tick4:989179968904904774>",
+                        value: "\u200B",
+                        inline: true,
+                    },
+                    {
+                        name: "Successfully created",
+                        value:
+                            `Awesome, all set up! Now let's jam some music together.\nJust paste any link or name in here ${channel}`,
+                        inline: true,
+                    },
+                    {
+                        name: "\u200B",
+                        value: "\u200B",
+                        inline: true,
+                    }
+                ]
+            })],
+            components: [
+                new MessageActionRow({
+                    components: [
+                        new MessageButton({
+                            customId: "abort",
+                            style: "SUCCESS",
+                            label: "Finish",
+                        })
+                    ]
+                })
+            ]
+        })
     }
 
     async getGuild(guildId: string) {
